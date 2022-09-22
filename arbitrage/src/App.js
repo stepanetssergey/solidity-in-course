c0ea013ca6318ace06306763509e85f773d94a10
Pinkmann <korgov.vova@icloud.com>
2022-09-22 21:33:05 +0200

    arbitrage upgrade

--- arbitrage/src/App.js
+++ arbitrage/src/App.js
@@ -12,9 +12,13 @@ import getContract from './services/getContract';
 import BigNumber from "bignumber.js";
 
 function App() {
-  const Factory = getContract(factory.abi, '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f');
+  const UniswapFactory = getContract(factory.abi, '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f');
+  const SushiswapFactory = getContract(factory.abi, '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac');
 
-  const [pairAddress, setPairAddress] = useState('');
+  const [uniswapPairAddress, setUniswapPairAddress] = useState('');
+
+  const [sushiswapPairAddress, setSushiswapPairAddress] = useState('');
+
   const [form, setForm] = useState({
     token0: '',
     token1: ''
@@ -26,13 +30,15 @@ function App() {
   ]);
 
   const [prices, setPrices] = useState({
-    token0: '',
-    token1: ''
+    uniswap: { token0: '', token1: '' },
+    sushiswap: { token0: '', token1: '' }
   });
 
   async function onSubmit() {
-    const response = await Factory.methods.getPair(form.token0, form.token1).call();
-    setPairAddress(response);
+    const uniswapResponse = await UniswapFactory.methods.getPair(form.token0, form.token1).call();
+    const sushiswapResponse = await SushiswapFactory.methods.getPair(form.token0, form.token1).call();
+    setUniswapPairAddress(uniswapResponse);
+    setSushiswapPairAddress(sushiswapResponse);
     const token0Contract = getContract(erc20.abi, form.token0);
     const token1Contract = getContract(erc20.abi, form.token1);
     const token0Name = await token0Contract.methods.name().call();
@@ -40,29 +46,74 @@ function App() {
     setTokens([{ name: token0Name }, { name: token1Name }]);
   }
 
-  async function handlePrices({ target }) {
+  async function handlePrices({ target }, type) {
     const otherToken = target.name === 'token0' ? 'token1' : 'token0';
-    setPrices((state) => ({ ...state, [target.name]: target.value }));
+    const otherType = type === 'uniswap' ? 'sushiswap' : 'uniswap';
+
+    setPrices((state) => ({
+      ...state,
+      [type]: {
+        ...state[type],
+        [target.name]: target.value
+      },
+      [otherType]: {
+        ...state[otherType],
+        [target.name]: target.value
+      }
+    }));
+
     if (!target.value) return;
+
     const decimals = await getDecimals();
-    const amount = await getAmount(target, decimals);
-    setPrices(state => ({ ...state, [otherToken]: (amount / 10 ** decimals[otherToken]).toFixed(2) }));
+    const amounts = await getAmounts(target, decimals);
+
+    setPrices(state => ({
+      ...state,
+      [type]: {
+        ...state[type],
+        [otherToken]: (amounts[type] / 10 ** decimals[otherToken]).toFixed(2)
+      },
+      [otherType]: {
+        ...state[otherType],
+        [otherToken]: (amounts[otherType] / 10 ** decimals[otherToken]).toFixed(2)
+      }
+    }));
   }
 
-  async function getAmount(target, decimals) {
-    const Router = getContract(router.abi, '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D');
-    const Pair = getContract(pairAbi.abi, pairAddress);
-    const reserves = await Pair.methods.getReserves().call();
+  async function getAmounts(target, decimals) {
+    const UniswapRouter = getContract(router.abi, '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D');
+    const SushiswapRouter = getContract(router.abi, '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F');
+
+    const uniswapReserves = await getReserves(uniswapPairAddress);
+    const sushiswapReserves = await getReserves(sushiswapPairAddress);
 
     const value = new BigNumber(target.value);
 
-    let amount;
+    let uniswap;
+    let sushiswap;
+
     if (target.name === 'token0') {
-      amount = await Router.methods.getAmountOut(value.multipliedBy(10 ** decimals.token0), reserves[0], reserves[1]).call();
+      uniswap = await getAmountOut(UniswapRouter, value, decimals, uniswapReserves);
+      sushiswap = await getAmountOut(SushiswapRouter, value, decimals, sushiswapReserves);
     } else {
-      amount = await Router.methods.getAmountIn(value.multipliedBy(10 ** decimals.token1), reserves[0], reserves[1]).call();
+      uniswap = await getAmountIn(UniswapRouter, value, decimals, uniswapReserves);
+      sushiswap = await getAmountIn(SushiswapRouter, value, decimals, sushiswapReserves);
     }
-    return amount;
+
+    return { uniswap, sushiswap };
+  }
+
+  async function getAmountIn(Router, value, decimals, reserves) {
+    return Router.methods.getAmountIn(value.multipliedBy(10 ** decimals.token1), reserves[0], reserves[1]).call();
+  }
+
+  async function getAmountOut(Router, value, decimals, reserves) {
+    return Router.methods.getAmountOut(value.multipliedBy(10 ** decimals.token0), reserves[0], reserves[1]).call();
+  }
+
+  async function getReserves(pairAddress) {
+    const Pair = getContract(pairAbi.abi, pairAddress);
+    return Pair.methods.getReserves().call();
   }
 
   async function getDecimals() {
@@ -78,7 +129,7 @@ function App() {
       <Header />
       <main className="main container">
         <Form form={form} setForm={setForm} onSubmit={onSubmit} />
-        {pairAddress &&
+        {(uniswapPairAddress || sushiswapPairAddress) &&
           <Prices tokens={tokens} prices={prices} setPrices={handlePrices} />
         }
       </main>
--- arbitrage/src/components/Prices.js
+++ arbitrage/src/components/Prices.js
@@ -4,8 +4,18 @@ export default function Prices({ prices, setPrices, tokens }) {
   return (
     <div className='form form--prices'>
       <h1 className="form__title">Prices</h1>
-      <input type="number" className="form__input" name="token0" value={prices.token0} placeholder={tokens[0].name} onChange={setPrices} />
-      <input type="number" className="form__input" name="token1" value={prices.token1} placeholder={tokens[1].name} onChange={setPrices} />
+      <div className="form__column">
+        <label className="form__label">Uniswap {tokens[0].name}</label>
+        <input type="number" className="form__input" name="token0" value={prices.uniswap.token0} placeholder={tokens[0].name} onChange={(e) => setPrices(e, 'uniswap')} />
+        <label className="form__label form__label--mt">Uniswap {tokens[1].name}</label>
+        <input type="number" className="form__input" name="token1" value={prices.uniswap.token1} placeholder={tokens[1].name} onChange={(e) => setPrices(e, 'uniswap')} />
+      </div>
+      <div className="form__column">
+        <label className="form__label">Sushiswap {tokens[0].name}</label>
+        <input type="number" className="form__input" name="token0" value={prices.sushiswap.token0} placeholder={tokens[0].name} onChange={(e) => setPrices(e, 'sushiswap')} />
+        <label className="form__label form__label--mt">Sushiswap {tokens[1].name}</label>
+        <input type="number" className="form__input" name="token1" value={prices.sushiswap.token1} placeholder={tokens[1].name} onChange={(e) => setPrices(e, 'sushiswap')} />
+      </div>
     </div>
   )
 }
